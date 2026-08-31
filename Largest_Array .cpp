c771dd21157138fd5494195ce00b5052568da7d6
Pintu Sardar <pintu@gmail.com>
2026-08-31 22:45:05 +0530

    add

--- Largest_Array .cpp
+++ Largest_Array .cpp
@@ -1,3 +1,19 @@
 //
 // Created by spider on 31-08-2026.
 //
+#include <iostream>
+using namespace std;
+int main () {
+
+    int arr[]={1,8,7,56,90};
+    int largest = arr[0];
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for ( int i = 0; i <n; i++) {
+        if ( arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    cout <<"Largest : " << largest;
+
+    return 0;
+}
